2dacdd9b650b725a4014bcdacdf8cbb15d8c5443
EduSanSou <eduardosantanasouza300@gmail.com>
2026-08-18 19:33:13 +0000

    Fix number of inputs

--- NSL-KDD/models/classifiers.py
+++ NSL-KDD/models/classifiers.py
@@ -116,7 +116,7 @@ def compare(x_old, y_old, x_test, y_test, data_generator, label_mapping, models,
                 assert rand_noise_dim > 0
                 nameUpsampler = 'RELU'
                 noise = np.random.normal(0, 1, (n, rand_noise_dim))
-                generated_x = data_generator(noise, labels).numpy()
+                generated_x = data_generator([noise, labels]).numpy()
                 generated_x = generated_x[:, :x_old.shape[1]]
             new_trainx = np.vstack([x_old,generated_x])
             new_y = np.append(y_old,labels)
